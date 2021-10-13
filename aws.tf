b6c50946c0bd0515b84d25bf702e18b5199967f3
pxsushil <sushil.chandra@tothenew.com>
2021-10-13 14:27:39 +0530

    Use s3 backend as well as dynamodb locking

--- aws.tf
+++ aws.tf
@@ -1,5 +1,14 @@
 provider "aws"{
-  access_key = "AKIA45MHVIDN2GNQ2MMV"
-  secret_key = "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"
+  # access_key = "AKIA45MHVIDN2GNQ2MMV"
+  # secret_key = "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"
   region     = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    encrypt = true    
+    bucket = "s3professorxplorer"
+    dynamodb_table = "Terraform_Table"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
